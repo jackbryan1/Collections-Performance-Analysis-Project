c233d183316de3ec63bfdf06dfab170c39624c03
b9015402 <b9015402@newcastle.ac.uk>
2020-02-18 12:59:41 +0000

    HashMapDirectory complete

--- HashMapDirectory.java
+++ HashMapDirectory.java
@@ -1,2 +1,87 @@
-public class HashMapDirectory {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.*;
+
+public class HashMapDirectory implements Directory{
+
+    private HashMap<String, Entry> entriesNum = new HashMap<>();
+    private HashMap<String, Entry> entriesStr = new HashMap<>();
+
+    public static void main(String args[]) throws FileNotFoundException {
+        HashMapDirectory hmd = new HashMapDirectory();
+        FileReader fr = new FileReader("test_data.csv");
+        Scanner s = new Scanner(fr).useDelimiter(",");
+        while(s.hasNext()){
+            hmd.insertEntry(new Entry(s.next(), s.next(), s.nextLine()));
+        }
+        s.close();
+        System.out.println(hmd.lookupExtension("Prott"));
+        hmd.deleteEntryUsingName("Amner");
+        hmd.deleteEntryUsingExtension("49521");
+        hmd.updateExtensionUsingName("Relph", "00000");
+        for(String k : hmd.entriesNum.keySet()){
+            System.out.println(hmd.entriesNum.get(k).getSurname() + hmd.entriesNum.get(k).getInitials() + hmd.entriesNum.get(k).getExtension());
+        }
+    }
+    /**
+     * Insert a new entry into the directory.
+     *
+     * @param entry the new entry to add
+     */
+    public void insertEntry(Entry entry) {
+        entriesNum.put(entry.getExtension(), entry);
+        entriesStr.put(entry.getSurname(), entry);
+    }
+
+    /**
+     * Remove an entry from the directory using their surname.
+     *
+     * @param surname the surname of the entry to remove
+     */
+    public void deleteEntryUsingName(String surname){
+        entriesNum.remove(entriesStr.get(surname).getExtension());
+        entriesStr.remove(surname);
+    }
+
+    /**
+     * Remove an entry from the directory using their extension number.
+     *
+     * @param number the extension number of the entry to remove
+     */
+    public void deleteEntryUsingExtension(String number){
+        entriesStr.remove(entriesNum.get(number).getSurname());
+        entriesNum.remove(number);
+    }
+
+    /**
+     * Update an entry's extension number using their surname.
+     *
+     * @param surname   surname of the entry to be updated
+     * @param newNumber the new number
+     */
+    public void updateExtensionUsingName(String surname, String newNumber){
+        entriesNum.get(entriesStr.get(surname).getExtension()).setExtension(newNumber);
+        entriesStr.get(surname).setExtension(newNumber);
+    }
+
+    /**
+     * Get the extension number of an entry using their surname.
+     *
+     * @param surname the surname of the entry
+     * @return the extension number of the entry
+     */
+    public String lookupExtension(String surname){
+        return entriesStr.get(surname).getExtension();
+    }
+
+    /**
+     * Return an array list of all entries in the directory.
+     *
+     * @return an array list of all entries
+     */
+    public List<Entry> toArrayList() {
+        Collection<Entry> temp = entriesNum.values();
+        ArrayList<Entry> arrayList = new ArrayList<>(temp);
+        return arrayList;
+    }
 }
